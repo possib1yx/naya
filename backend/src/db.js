096583abf7dd4073e7f71d78c9f065308858fed6
possib1yx <dahalbinayak36@gmail.com>
2026-03-08 08:30:45 +0545

    Update db.js

--- backend/src/db.js
+++ backend/src/db.js
@@ -2,7 +2,18 @@ const admin = require('firebase-admin');
 const path = require('path');
 require('dotenv').config();
 
-const serviceAccount = require('../firebase-service-account.json');
+let serviceAccount;
+
+if (process.env.FIREBASE_SERVICE_ACCOUNT) {
+  try {
+    serviceAccount = JSON.parse(process.env.FIREBASE_SERVICE_ACCOUNT);
+  } catch (error) {
+    console.error('Error parsing FIREBASE_SERVICE_ACCOUNT env variable:', error);
+    serviceAccount = require('../firebase-service-account.json');
+  }
+} else {
+  serviceAccount = require('../firebase-service-account.json');
+}
 
 if (!admin.apps.length) {
   admin.initializeApp({
